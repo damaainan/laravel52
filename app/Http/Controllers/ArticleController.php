73c5856270464c53e710b7d8692568e1775e46b4
damaainan <damaainan@163.com>
2017-02-18 23:10:24 +0800

    comment

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
+use App\Article;
 class ArticleController extends Controller
 {
     public function show($id)
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
+use App\Comment;
 class CommentController extends Controller
 {
     public function store(Request $request)
